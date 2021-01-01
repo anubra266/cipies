0edad8300ea9b0d7b8051fd0794cc4504b8fd84a
anubra266 <anubra266@gmail.com>
2021-01-01 04:58:23 +0100

    Publish Fortify Views

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('home', 'home')
+	->name('home')
+	->middleware(['auth', 'verified']);
