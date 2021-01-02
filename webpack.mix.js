1976cc3fb3c73969efc3a03aeac20925f9839dbc
anubra266 <anubra266@gmail.com>
2021-01-02 20:35:39 +0100

    Revert "Add Kutty to tailwind config"
    This reverts commit 4bf4c3fb2ccf720fd77072fb7389c1dc5df8952b.

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,7 @@ const mix = require("laravel-mix");
 
 mix.react("resources/js/app.js", "public/js")
     .sass("resources/sass/app.scss", "public/css")
+    .postCss("resources/css/app.css", "public/css", [require("tailwindcss")])
     .webpackConfig({
         output: { chunkFilename: "js/[name].js?id=[chunkhash]" },
         resolve: {
