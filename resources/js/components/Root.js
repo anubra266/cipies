820e21bd4c0415e22e8ff660c798aa514c1d0d2b
anubra266 <anubra266@gmail.com>
2021-01-04 04:34:01 +0100

    Import Antd CSS

--- resources/js/components/Root.js
+++ resources/js/components/Root.js
@@ -3,6 +3,7 @@ import React from "react";
 import { render } from "react-dom";
 
 // import 'bootstrap/dist/css/bootstrap.css';
+import 'antd/dist/antd.css';
 import { useProgressBar } from "./ProgressBar";
 const el = document.getElementById("app");
 
