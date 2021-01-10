39686c0c7884fcb13977693900cfb82fe45319ea
anubra266 <anubra266@gmail.com>
2021-01-10 23:11:15 +0100

    Add Chakra UI

--- resources/js/Root.js
+++ resources/js/Root.js
@@ -1,20 +1,26 @@
 import { App } from "@inertiajs/inertia-react";
 import React from "react";
 import { render } from "react-dom";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
-// import 'bootstrap/dist/css/bootstrap.css';
-import "antd/dist/antd.css";
+// import "antd/dist/antd.css";
 import "../css/app.css";
 import { useProgressBar } from "./components/ProgressBar";
 const el = document.getElementById("app");
-
+const config = {
+    useSystemColorMode: false,
+    initialColorMode: "dark"
+};
+const customTheme = extendTheme({ config });
 useProgressBar();
 render(
-    <App
-        initialPage={JSON.parse(el.dataset.page)}
-        resolveComponent={name =>
-            import(`~/Pages/${name}`).then(module => module.default)
-        }
-    />,
+    <ChakraProvider theme={customTheme}>
+        <App
+            initialPage={JSON.parse(el.dataset.page)}
+            resolveComponent={name =>
+                import(`~/Pages/${name}`).then(module => module.default)
+            }
+        />
+    </ChakraProvider>,
     el
 );
